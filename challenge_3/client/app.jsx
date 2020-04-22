03b7456ddad74e351af2ccac1daba252b59342fc
Breg Bromley <bregbromley@gmail.com>
2020-04-22 14:43:00 -0600

    app.js rendering to the dom, began work on schema'

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,17 @@
 
+class App extends React.Component {
+  constructor(props) {
+    super(props) ;
+  }
 
+  render() {
+    console.log('I ran');
+    return (
+      <div>
+        I AM RENDERING!!!!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const port = 3000;
+const fs = require('fs');
+const path = require('path');
+
+app.use(express.static('public'));
+app.use(bodyParser());
+
+app.get('/', (req, res) => {
+  console.log('get received');
+  res.send('get received');
+})
+
+app.post('/', (req, res) => {
+  console.log('post received');
+  res.send('post received');
+})
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}.......`));
 
 
